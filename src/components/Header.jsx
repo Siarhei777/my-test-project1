bd6f1fa39e916fc51255064e2b5bbb587c80a4e0
Siarhei Zhorau <jorov77@mail.ru>
2020-11-18 22:12:28 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='./assets/images/pic1.jpg' alt='Картинка1' />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+        <img src="./assets/images/pic2.jpg" alt="Картинка 2" className="img1" />
+        <img src="./assets/images/pic3.jpg" alt="Картинка 2" className="img2" />
+        <p className="description">Description</p>
+        <div>My post</div>
+        <div>New post</div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
+    );
+}
+
+export default Profile;
